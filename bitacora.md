68b1314bb2ba38c638a15caaed3d0a9ac2557c65
Natalia Tsyleva <tsylevanatalia@gmail.com>
2024-07-04 16:56:24 +0200

    feat: a romano completa. Puede refactorizarse.DRY

--- bitacora.md
+++ bitacora.md
@@ -5,3 +5,10 @@
 to_roman(n: int) -> str
 ```
 - Test de unidades simples
+- Test de numeros que suman de mayor a menor
+
+Despues de la investigacion hemos decidido que un buen algoritmo es 
+    1. Dividir el numero(siempre menor de 4000) en millares, centenas decenas y unidades. Poner en una lista
+    2. Procesar cada item de la lista transformandolo en romano. Tendre que retocar al algoritmo que ya tengo.
+    3. Concatenar de mayor a menor los simbolos romanos obtenidos.
+
--- test_roman_number.py
+++ test_roman_number.py
@@ -1,4 +1,4 @@
-from roman_funcs import to_roman
+from roman_funcs import to_roman, dividir_en_digitos, arabigo_a_romano
 
 def test_romanes_simples():
     assert to_roman(1) == "I"
@@ -31,4 +31,12 @@ def test_romanos_10_al_90():
     assert to_roman(80) == "LXXX"
     assert to_roman(90) == "XC"        
 
+def test_dividir_en_digitos():
+
+    assert dividir_en_digitos(34) == [0, 0, 30, 4]
+    assert dividir_en_digitos(2024) == [2000, 0, 20, 4]
+    assert dividir_en_digitos(100) == [0, 100, 0, 0]
+
+def test_cualquier_romano():
+    assert arabigo_a_romano(1999) =="MCMXCIX"
     
